25b5689c97b32b5607a9c4fc35006c5745e693c5
Seth Kester <seth@noblehost.org>
2024-05-07 16:44:41 -0500

    commit 6

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     myClientApp.alerting.showToastPopup('Hello', 'Genesys Cloud');
 
-    // Log the environment to the console
-    console.log(myClientApp.gcEnvironment);
+    // Function to get parameter value from URL
+    function getParamValue(paramPairs){
+        return decodeURIComponent(paramPairs[1] || '');
+    }
 
-    // Display the environment in the web page
+    var queryString = window.location.search.substring(1);
+    var pairs = queryString.split('&');
+    var conversationId = null;
+    var gcHostOrigin = null;
+    var gcTargetEnv = null;
+
+    for (let i = 0; i < pairs.length; i++) {
+        let currParam = pairs[i].split('=');
+       
+        if(['conversationId', 'gcConversationId'].indexOf(currParam[0]) !== -1) {
+            conversationId = getParamValue(currParam);
+        } else if (currParam[0] === 'gcHostOrigin') {
+            gcHostOrigin = getParamValue(currParam);
+        } else if (currParam[0] === 'gcTargetEnv') {
+            gcTargetEnv = getParamValue(currParam);
+        }
+    }
+
+    // Display the environment and conversation ID in the web page
     var dataContainer = document.getElementById('data-container');
-    dataContainer.textContent = 'Current Environment: ' + myClientApp.gcEnvironment;
+    dataContainer.innerHTML = '<strong>Current Environment:</strong> ' + myClientApp.gcEnvironment +
+                              '<br><strong>Conversation ID:</strong> ' + (conversationId || 'N/A');
 });
